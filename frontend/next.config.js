3b96736d905b1b7bf97c88c5090e3b05f9ab9afd
fak3alien <alienwhowasfaked@gmail.com>
2022-11-27 19:40:52 +0100

    add tailwind & setup environment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['ts', 'tsx'],
+  publicRuntimeConfig: {
+    ...Object.fromEntries(Object.entries(process.env).filter(([key]) => key.startsWith('NEXT_PUBLIC_'))),
+  },
 }
 
 module.exports = nextConfig
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-green-300 h-10">test</div>
+}
